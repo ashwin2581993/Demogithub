5404295d8811026953cf538276f7e1c60a9bc596
ashwin2581993 <ashwin2581993@gmail.com>
2020-12-04 13:26:28 +0530

    home file add

---
+++ src/main/java/com/qa/pages/home.java
@@ -0,0 +1,13 @@
+package com.qa.pages;
+
+public class home {
+	
+	
+	public void test() {
+		System.out.println("print a");
+	}
+	
+	public void test1() {
+		System.out.println("print csddsd a");
+	}
+}
